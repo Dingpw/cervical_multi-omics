a7ca1953e4ef42659446f297df439af37e408e68
Dingpw <42132756+Dingpw@users.noreply.github.com>
2019-10-21 04:08:16 +0800

    Update HiC-Pro_step2.sh

--- HiC-Pro_step2.sh
+++ HiC-Pro_step2.sh
@@ -1,5 +1,5 @@
-/software/HiC-Pro \
---input /HiC_pro/hicpro_output/bowtie_results/bwt2 \
+HiC-Pro \
+--input hicpro_output/bowtie_results/bwt2 \
 --output hicpro_output_2 \
 --conf ./config-hicpro.txt \
 -s proc_hic \
