eb16809492c9ea34c6fdd259bf73668e89b8a35d
Dingpw <42132756+Dingpw@users.noreply.github.com>
2019-10-20 18:53:44 +0800

    Update ATAC.sh

--- ATAC.sh
+++ ATAC.sh
@@ -1,3 +1,4 @@
+# We used pipeline 
 CODE=/software/atac_dnase_pipelines
 DATA=/cervical/ATAC-SEQ/upload/Rawdata
 WORK_ROOT=/cervical; SUFFIX=ATAC_cc; WORK=$WORK_ROOT/$SUFFIX
@@ -6,7 +7,7 @@ bds $CODE/atac.bds \
 -nth 32 \
 -enable_idr \
 -out_dir ./ATAC_out \
--fastq1_1 $DATA/ECT/ECT_R1.fq.gz \
--fastq1_2 $DATA/ECT/ECT_R2.fq.gz \
--fastq2_1 $DATA/Siha/Siha_R1.fq.gz \
--fastq2_2 $DATA/Siha/Siha_R2.fq.gz \
+-fastq1_1 $SAMPLE1_R1.fq.gz \
+-fastq1_2 $SAMPLE1_R2.fq.gz \
+-fastq2_1 $SAMPLE2_R1.fq.gz \
+-fastq2_2 $SAMPLE2_R2.fq.gz \
