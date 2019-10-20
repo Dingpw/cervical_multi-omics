06da3af1713b12f9ebff3f825d620a51a9c12e3f
Dingpw <42132756+Dingpw@users.noreply.github.com>
2019-10-20 19:05:00 +0800

    Update step3.sh

--- step3.sh
+++ step3.sh
@@ -1,5 +1,5 @@
-/zfssz2/ST_MCHRI/COHORT/zhongjixing/software/HiC-Pro_2.11.1/bin/HiC-Pro \
---input /zfssz2/ST_MCHRI/COHORT/zhongjixing/Hic_cc/HiC_pro_2/hicpro_output_2/hic_results/data/ \
+/software/HiC-Pro \
+--input /Hic/HiC_pro/hicpro_output_2/hic_results/data/ \
 --output hicpro_output_3 \
 --conf ./config-hicpro.txt \
 -s merge_persample \
