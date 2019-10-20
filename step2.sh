0343e692f18968c94c54f35c350815c40d70486b
Dingpw <42132756+Dingpw@users.noreply.github.com>
2019-10-20 19:03:47 +0800

    Update step2.sh

--- step2.sh
+++ step2.sh
@@ -1,5 +1,5 @@
-/zfssz2/ST_MCHRI/COHORT/zhongjixing/software/HiC-Pro_2.11.1/bin/HiC-Pro \
---input /zfssz2/ST_MCHRI/COHORT/zhongjixing/Hic_cc/HiC_pro_2/hicpro_output/bowtie_results/bwt2 \
+/software/HiC-Pro \
+--input /HiC_pro/hicpro_output/bowtie_results/bwt2 \
 --output hicpro_output_2 \
 --conf ./config-hicpro.txt \
 -s proc_hic \
