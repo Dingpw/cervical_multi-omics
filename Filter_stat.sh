1cadea7365b98180f428c8a72894ce45ce4976c5
Dingpw <42132756+Dingpw@users.noreply.github.com>
2019-10-20 21:58:58 +0800

    Delete Filter_stat.sh

--- Filter_stat.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo ==========start at : `date` ==========
-perl /ifs4/BC_PUB/biosoft/pipeline/RNA/RNA_RNAseq/RNA_RNAseq_2017a/Filter/filter_stat.pl -indir /hwfssz5/ST_MCHRI/COHORT/USER/zhongjixing/DPW/RNA-seq/result_2/process/Filter_SOAPnuke -output /hwfssz5/ST_MCHRI/COHORT/USER/zhongjixing/DPW/RNA-seq/result_2/process/Filter_SOAPnuke/FilterSummary.xls && \
-cp /hwfssz5/ST_MCHRI/COHORT/USER/zhongjixing/DPW/RNA-seq/result_2/process/Filter_SOAPnuke/FilterSummary.xls /hwfssz5/ST_MCHRI/COHORT/USER/zhongjixing/DPW/RNA-seq/result_2/Analysis_Report/BGI_result/CleanData && \
-cd /hwfssz5/ST_MCHRI/COHORT/USER/zhongjixing/DPW/RNA-seq/result_2/upload/CleanData/../ && \
-md5sum CleanData/*fq.gz > md5.txt && \
-echo ==========end at : `date` ========== && \
-echo Still_waters_run_deep 1>&2 && \
-echo Still_waters_run_deep > /hwfssz5/ST_MCHRI/COHORT/USER/zhongjixing/DPW/RNA-seq/result_2/shell/Filter_SOAPnuke/Filter_stat.sh.sign
